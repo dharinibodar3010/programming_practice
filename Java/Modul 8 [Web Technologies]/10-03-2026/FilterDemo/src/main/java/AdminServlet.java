2ada2b13f015a429f4fed047c468837e42204a6e
dharinibodar3010 <dharinibodar3010@gmail.com>
2026-03-18 11:08:00 +0530

    commit

--- Java/Modul 8 [Web Technologies]/10-03-2026/FilterDemo/src/main/java/AdminServlet.java
+++ Java/Modul 8 [Web Technologies]/10-03-2026/FilterDemo/src/main/java/AdminServlet.java
@@ -13,7 +13,8 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/AdminServlet")
 public class AdminServlet extends HttpServlet 
 {   
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
 		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
 		response.setContentType("text/html");
