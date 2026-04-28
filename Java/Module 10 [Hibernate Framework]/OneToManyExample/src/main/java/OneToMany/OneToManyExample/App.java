b2b5cb75041fd9a8989677953a2c7e993b15c339
dharinibodar3010 <dharinibodar3010@gmail.com>
2026-04-28 11:40:12 +0530

    commit

---
+++ Java/Module 10 [Hibernate Framework]/OneToManyExample/src/main/java/OneToMany/OneToManyExample/App.java
@@ -0,0 +1,10 @@
+package OneToMany.OneToManyExample;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
