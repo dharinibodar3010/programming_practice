ba888ed8d886a6dcc93c366cdd9392fee9c331ca
dharinibodar3010 <dharinibodar3010@gmail.com>
2026-05-05 21:53:24 +0530

    commit

---
+++ Java/Module 11 [Spring Framework]/Spring_Constructor_Ex/src/main/java/spring/Spring_Constructor_Ex/App.java
@@ -0,0 +1,13 @@
+package spring.Spring_Constructor_Ex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
