2335755e4d1ae525f711c0d517221ac1ae42ce7c
dharinibodar3010 <dharinibodar3010@gmail.com>
2026-05-09 11:58:22 +0530

    commit

---
+++ Java/Module 11 [Spring Framework]/Spring_Autowiring/src/main/java/spring/Spring_Autowiring/App.java
@@ -0,0 +1,13 @@
+package spring.Spring_Autowiring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
